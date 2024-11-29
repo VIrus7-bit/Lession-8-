ef3d43f93ae2f50eb64e38740779ad7b5baf2f4d
VIrus7-bit <italyfotura7@gmail.com>
2024-11-29 09:46:32 -0800

    Lession 8

---
+++ Lession 8 Task 1.0/Lession 8 Task 1.0.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+#include <memory>
+
+class tridiagonal_matrix {
+public:
+    std::vector<double> m_down;
+    std::vector<double> m_upper;
+    std::vector<double> m_middle;
+
+    tridiagonal_matrix(
+        const std::vector<double>& down,
+        const std::vector<double>& upper,
+        const std::vector<double>& middle) :
+        m_down{ down }, m_upper{ upper }, m_middle{ middle }
+    {
+    };
+
+    ~tridiagonal_matrix() {
+        std::cout << "destructor called\n";
+    }
+
+    std::unique_ptr<tridiagonal_matrix> clone() const {
+        return std::make_unique<tridiagonal_matrix>(m_down, m_upper, m_middle);
+    }
+};
+
+int main() {
+
+    using namespace std;
+    setlocale(LC_ALL, "Rus");
+
+    auto down = vector<double>{ -4.0, 5.0 };
+    auto upper = vector<double>{ 14.0, 8.0 };
+    auto middle = vector<double>{ 3.0, 1.0, 7.0 };
+
+    auto matrix = make_unique<tridiagonal_matrix>(down, upper, middle);
+
+    auto matrix_clone = matrix->clone();
+
+    std::cout << "matrix_clone created successfully\n";
+
+    return EXIT_SUCCESS;
+}
---
+++ Lession 8 Task 2.0/Lession 8 Task 2.0.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <memory>
+
+class node {
+public:
+    int m_value;
+    std::weak_ptr<node> parent;
+
+    node(int value) : m_value{ value } {}
+    ~node() { std::cout << "destructor called\n"; }
+};
+
+int main() {
+    using namespace std;
+    setlocale(LC_ALL, "Rus");
+    
+    auto node1 = make_shared<node>(1);
+    auto node2 = make_shared<node>(2);
+
+    node1->parent = node2; // node1 ссылается на node2 через weak_ptr
+    node2->parent = node1; // node2 ссылается на node1 через weak_ptr
+
+    return EXIT_SUCCESS;
+}
